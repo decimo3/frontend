7f4ea6567427eed7f8a88df51e74e12aed631ed0
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-01 12:51:39 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export function App(){
+  return (
+    <div>
+      <h1>Welcome to REACT!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
