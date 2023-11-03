6b89f76ddbc230bb0c1465fddebf59bff6f718b8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-03 12:35:55 -0300

    REFACTOR: Renamed class

--- src/Funcionario/Read.js
+++ src/Funcionario/Read.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { baseURL } from "../Service";
+import { baseURL } from "../Environment";
 import { Link } from "react-router-dom";
 export default function ReadFuncionario()
 {
