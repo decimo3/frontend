2ff8c313ce59dbc758aa48d13f69ac58d4d668c8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-03 08:02:17 -0300

    Implemented react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Rodape from './Public/Rodape';
+import Header from './Public/Header';
+import Upload from './Upload';
+import Startup from './Startup';
+import ReadFuncionario from './Funcionario/Read';
 
-export function App(){
+export default function App(){
   return (
-    <div>
-      <h1>Welcome to REACT!</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Startup/>} />
+        <Route exact path='/Upload' element={<Upload/>} />
+        <Route exact path='/Funcionario' element={<ReadFuncionario/>} />
+        {/* <Route exact path='/Composicao' element={<Composicao/>} />
+        <Route exact path='/Relatorio' element={<Relatorio/>} /> */}
+      </Routes>
+      <Rodape /> 
+    </BrowserRouter>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.createRoot(document.getElementById('app')).render(<App/>);
