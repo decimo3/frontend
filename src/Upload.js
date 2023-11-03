2380df3fd9511a5230f916cf25a4e5e5206b38dc
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-03 08:03:11 -0300

    Added form for sending files

---
+++ src/Upload.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Upload()
+{
+  return (
+    <form action="/Uploader" method="post" className="card p-2 m-2">
+      <div className="form-group">
+        <label for="arquivo" className="my-2">Selecione o arquivo para enviar:</label>
+        <input className="form-control my-2" type="file" required accept=".xlsx, .csv" />
+      </div>
+      <input className="btn btn-primary btn-block my-2" type="submit" value="Enviar" />
+    </form>
+  )
+}
