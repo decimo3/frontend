24f801fc6b5be73e189e98d55d284ee7b01e0778
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-03 08:13:17 -0300

    Added header and footer components

---
+++ src/Public/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "react-router-dom";
+export default function Header()
+{
+  return (
+    <header>
+      <h1 className="card text-center p-2 m-2">
+        Sistema de atualização da produção
+      </h1>
+        <nav className="card text-center d-flex flex-row justify-content-around p-2 m-2">
+          <Link to='/'>Inicio</Link>
+          <Link to='/Upload'>Upload</Link>
+          <Link to='Funcionario'>Funcionários</Link>
+          <Link to='/Composicao'>Composições</Link>
+          <Link to='/Relatorio'>Relatórios</Link>
+        </nav>
+    </header>
+  )
+}
---
+++ src/Public/Rodape.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Rodape() {
+  return (
+    <footer className="card text-center p-2 m-2">
+      <p>Made with <span className="text-danger">&hearts;</span> by <a href="https://www.linkedin.com/in/ruanfaustinocamello/" target="_blank">MestreRuan</a>!</p>
+      <p>Repository: <a href="https://github.com/decimo3">decimo3/com.indicaservicos.producao.alimentador</a></p>
+    </footer>
+  );
+}
