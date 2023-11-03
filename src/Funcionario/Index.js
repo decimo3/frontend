ba452ad17da62f3b25c8c0aed2769d63359ee619
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-03 14:38:38 -0300

    Create index to route 'Funcionario' methods

---
+++ src/Funcionario/Index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import Read from "./Read";
+import Edit from "./Edit";
+export default function Funcionario()
+{
+  return (
+      <Routes>
+        <Route path="/" element={<Read/>} />
+        <Route path="/Edit" element={<Edit/>} />
+      </Routes>
+  );
+}
