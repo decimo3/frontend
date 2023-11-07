a8e7ea148b320072e8e92c412be2c5a721c24833
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-07 14:20:52 -0300

    Component 'edit' made reactive

--- src/Funcionario/Edit.js
+++ src/Funcionario/Edit.js
@@ -12,21 +12,17 @@ export default function Edit()
 {
   const [stateMat, setStateMat] = React.useState("d-none");
   const [stateNom, setStateNom] = React.useState("d-none");
-  React.useEffect(() => {
-    
-  }, [])
   let { state } = useLocation()
   return (
-    <main>
       <form className="form-control">
         <div className="form-group my-2">
           <label>Matrícula Light:</label>
-          <input className="form-control" type="number" value={state.matricula} onChange={setStateMat(isValidMatricula)}/>
+          <input className="form-control" type="number" value={state.matricula}/>
           <p className={stateMat}>O matrícula digitada é inválida!</p>
         </div>
         <div className="form-group py-2">
           <label>Nome do colaborador:</label>
-          <input type="text" className="form-control" value={state.nome_colaborador} onChange={setStateNom(isValidMatricula)}/>
+          <input type="text" className="form-control" value={state.nome_colaborador}/>
           <p className={stateNom}>O nome digitado é inválido!</p>
         </div>
         <div className="form-group py-2">
@@ -38,8 +34,8 @@ export default function Edit()
         </div>
         <div className="form-group py-2">
           <input type="button" className="btn btn-primary btn-block" value="Enviar"/>
+          <input type="button" className="btn btn-danger btn-block" value="Excluir"/>
         </div>
       </form>
-    </main>
   );
 }
