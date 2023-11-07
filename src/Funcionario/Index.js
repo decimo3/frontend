cea4a76000bc7236c7c307effda9e01e47b9b357
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-07 14:21:25 -0300

    Added 'create' component

--- src/Funcionario/Index.js
+++ src/Funcionario/Index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Read from "./Read";
 import Edit from "./Edit";
+import Create from "./Create";
 export default function Funcionario()
 {
   return (
       <Routes>
         <Route path="/" element={<Read/>} />
         <Route path="/Edit" element={<Edit/>} />
+        <Route path="/Create" element={<Create/>} />
       </Routes>
   );
 }
