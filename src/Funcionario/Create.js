b86bf17973bbc3daede3be1dfc4f0471912daa52
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-07 14:21:10 -0300

    Created 'create' component

---
+++ src/Funcionario/Create.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { baseURL } from "../Environment";
+
+export default function Create()
+{
+  async function newFuncionario(mat, nom, fun)
+  {
+    const data = JSON.stringify({
+      matricula: mat,
+      nome_colaborador: nom,
+      funcao: Number(fun)
+    })
+    const req = {
+      method: "POST",
+      body: data,
+      headers: new Headers({
+        'Content-Type': 'application/json; charset=UTF-8'
+      })
+    }
+    const res = await fetch(`${baseURL}/Funcionario`, req)
+      .then((r) => {
+        console.dir(r);
+        return "aceitado";
+      })
+      .catch((r) => {
+        console.error(r);
+        return "rejeitado";
+      });
+    return res;
+  }
+  const history = useNavigate();
+  const [mat, setMat] = React.useState("");
+  const [stateMat, setStateMat] = React.useState('d-none');
+  const [nom, setNom] = React.useState("");
+  const [stateNom, setStateNom] = React.useState('d-none');
+  const [fun, setFun] = React.useState("");
+  const [stateFun, setStateFun] = React.useState('d-none');
+  const [redirect, setRedirect] = React.useState(null);
+  React.useEffect(() => {
+    console.log(redirect);
+    if (redirect === "aceitado") history('/Create');
+    if (redirect === "rejeitado") history('/Index');
+  }, [redirect]);
+  return (
+    <form className="card p-2 m-2">
+      <div className="form-group my-2">
+        <label>Matrícula Light:</label>
+        <input className="form-control" type="number" value={mat} onChange={(e) => {setMat(e.target.value);}}/>
+        <p className={stateMat}>O matrícula digitada é inválida!</p>
+      </div>
+      <div className="form-group py-2">
+        <label>Nome do colaborador:</label>
+        <input type="text" className="form-control" value={nom} onChange={(e) => {setNom(e.target.value);}}/>
+        <p className={stateNom}>O nome digitado é inválido!</p>
+      </div>
+      <div className="form-group py-2">
+        <label>Função do colaborador:</label>
+        <select value={fun} className="form-control" onChange={(e) => {setFun(e.target.value);}}>
+          <option value="0">Eletricista</option>
+          <option value="1">Supervisor</option>
+        </select>
+        <p className={stateFun}>A função escolhida não é válida!</p>
+      </div>
+      <div className="form-group py-2">
+        <input type="button" className="btn btn-primary btn-block" value="Enviar" onClick={() => {setRedirect(newFuncionario(mat, nom, fun))}}/>
+        <input type="button" className="btn btn-white btn-block border" value="Voltar" onClick={() => {history('/Funcionario');}} />
+      </div>
+    </form>
+  );
+}
