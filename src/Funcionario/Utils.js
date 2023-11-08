1da1dc066b410691c93ebc181e9f6adcf760265a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-08 12:47:21 -0300

    1. regular expression did not limit the number of characters due to the lack of an anchor 2. regular expression validated any number of words and required space at the end; 3. The functions did not return boolean, as expected in the component.

--- src/Funcionario/Utils.js
+++ src/Funcionario/Utils.js
@@ -1,9 +1,9 @@
 export const funcoes = ["Eletricista", "Supervisor"];
 export function isValidMatricula(mat)
 {
-  return (RegExp(/[0-9]{7}/).test(mat)) ? "d-none" : "invalid-feedback";
+  return RegExp(/^[0-9]{7}$/).test(mat);
 }
 export function isValidName(nom)
 {
-  return (RegExp(/[A-Z ]{4,60}/).test(nom)) ? "d-none" : "invalid-feedback";
+  return RegExp(/^([A-z\s]){2,}([A-z]){1}$/).test(nom);
 }
