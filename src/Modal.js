5e619295b9c91d7e8cf16ff92924574b5d641422
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-08 12:49:30 -0300

    Added modal

---
+++ src/Modal.js
@@ -0,0 +1,28 @@
+import React from "react";
+export default function Modal({ listaAvisos, onClose })
+{
+  const [showModal, setShowModal] = React.useState(true);
+  const closeModal = () => {onClose(); setShowModal(false);}
+  return showModal ? (
+    <div className="modal-backdrop" onClick={() => {closeModal();}}>
+      <div className="card position-absolute top-50 start-50 translate-middle">
+        <div className="modal-dialog" onClick={() => {closeModal();}}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button className="close-btn" onClick={() => {closeModal();}}>&times;</button>
+            </div>
+            <hr/>
+            <div className="modal-body">
+              {listaAvisos.map((li) => (<p className="text-danger">{li}</p>))}
+            </div>
+            <hr/>
+            <div className="modal-footer">
+            <button className="close-btn" onClick={() => {closeModal();}}>OK</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : null;
+}
