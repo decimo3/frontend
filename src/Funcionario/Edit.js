281c005bc30c37c5ff2def67c2fc4f51a00b96f9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-08 13:11:25 -0300

    component reactivity implemented correctly

--- src/Funcionario/Edit.js
+++ src/Funcionario/Edit.js
@@ -3,24 +3,23 @@ import { useLocation } from "react-router-dom";
 
 export default function Edit()
 {
-  const [stateMat, setStateMat] = React.useState("d-none");
-  const [stateNom, setStateNom] = React.useState("d-none");
   let { state } = useLocation()
+  const [stateMat, setMat] = React.useState(state.matricula);
+  const [stateNom, setNom] = React.useState(state.nome_colaborador);
+  const [stateFun, setFun] = React.useState(state.funcao);
   return (
       <form className="form-control">
         <div className="form-group my-2">
           <label>Matrícula Light:</label>
-          <input className="form-control" type="number" value={state.matricula}/>
-          <p className={stateMat}>O matrícula digitada é inválida!</p>
+          <input className="form-control" type="number" value={stateMat} onChange={(e) => {setMat(e.target.value);}} required/>
         </div>
         <div className="form-group py-2">
           <label>Nome do colaborador:</label>
-          <input type="text" className="form-control" value={state.nome_colaborador}/>
-          <p className={stateNom}>O nome digitado é inválido!</p>
+          <input type="text" className="form-control" value={stateNom} onChange={(e) => {setNom(e.target.value);}} required/>
         </div>
         <div className="form-group py-2">
           <label>Função do colaborador:</label>
-          <select className="form-control">
+          <select value={stateFun} className="form-control" onChange={(e) => {setFun(e.target.value);}}>
             <option value="0">Eletricista</option>
             <option value="1">Supervisor</option>
           </select>
