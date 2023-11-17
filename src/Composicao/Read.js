9287da2e6c26f2fd303146e012e7ce9e8b331797
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-17 12:26:09 -0300

    1. Added states to filter the list of compositions; 2. Added inputs to change filter states; 3. Added composition list filter; 4. Removed last names to fit the screen;

--- src/Composicao/Read.js
+++ src/Composicao/Read.js
@@ -4,6 +4,10 @@ import { baseURL } from "../Environment";
 import { atividade, regional } from './Utils';
 export default function Read() {
   const [comp, setComp] = React.useState([]);
+  const [dataStart, setDataStart] = React.useState(Date(Date.now()));
+  const [dataStop, setDataStop] = React.useState(Date(Date.now()));
+  const [ativ, setAtiv] = React.useState("");
+  const [reg, setReg] = React.useState("");
   React.useEffect(() => {
     async function getComposicoes()
     {
@@ -22,7 +26,24 @@ export default function Read() {
   }
   return (
     <main className="card p-2 m-2">
-    <Link to='Create'>Criar nova composição</Link>
+    <div className="d-flex justify-content-around">
+      <Link to='Create'>Criar nova composição</Link>
+      <label>Data inicio:</label>
+      <input type="date" value={dataStart} onChange={(d) => { setDataStart(d.target.value); }}/>
+      <label>Data final:</label>
+      <input type="date" value={dataStop} onChange={(d) => { setDataStop(d.target.value); }}/>
+      <label>Tipo atividade:</label>
+      <select defaultValue={ativ} onChange={(e) => {setAtiv(e.target.value)}}>
+        <option value={""}>TODAS</option>
+        {atividade.map((r, i) => ( <option value={i} key={i}>{r}</option> ))}
+      </select>
+      <label>Qual Regional:</label>
+      <select defaultValue={reg} onChange={(e) => {setReg(e.target.value)}}>
+        <option value={""}>TODAS</option>
+        {regional.map((r, i) => ( <option value={i} key={i}>{r}</option> ))}
+      </select>
+    </div>
+    <hr/>
       <table className="table table-hover">
         <thead>
           <tr>
@@ -43,21 +64,23 @@ export default function Read() {
           </tr>
         </thead>
         <tbody>
-          {}
-          {comp.map((c) => (
+          {comp.filter((c) => (
+            (new Date(c.dia) >= new Date(dataStart)) &&
+            (new Date(c.dia) <= new Date(dataStop))
+          )).map((c) => (
             <tr scope="row" key={c.dia + c.recurso}>
-              <td>{c.dia}</td>
+              <td>{c.dia.substr(0, 10).split('-').reverse().join('/')}</td>
               <td>{c.adesivo}</td>
               <td>{c.placa}</td>
               <td>{c.recurso}</td>
               <td>{atividade[Number(c.atividade)]}</td>
               <td>{c.id_motorista}</td>
-              <td>{c.motorista}</td>
+              <td>{c.motorista.split(" ", 1)[0]}</td>
               <td>{c.id_ajudante}</td>
-              <td>{c.ajudante}</td>
+              <td>{c.ajudante.split(" ", 1)[0]}</td>
               <td>{c.telefone}</td>
               <td>{c.id_supervisor}</td>
-              <td>{c.supervisor}</td>
+              <td>{c.supervisor.split(" ", 1)[0]}</td>
               <td>{regional[Number(c.regional)]}</td>
               <td>
                 <Link to="Edit" state={c}>Editar</Link>
