b3c1c23bd454eeae8292bbbcea0c470791fbd412
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-17 15:02:53 -0300

    Added path to composition edit page

--- src/Composicao/Index.js
+++ src/Composicao/Index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Read from "./Read";
+import Edit from "./Edit";
 export default function Composicao() {
   return (
     <Routes>
       <Route path="/" element={<Read/>}/>
+      <Route path="/Edit" element={<Edit/>}/>
     </Routes>
   );
 }
