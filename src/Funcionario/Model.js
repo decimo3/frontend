92e6d07bb254f1fc660d604adbd47f6dee6bf45b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-22 14:32:55 -0300

    Added class for business domain

--- src/Funcionario/Model.js
+++ src/Funcionario/Model.js
@@ -7,3 +7,16 @@ export function isValidName(nom)
 {
   return RegExp(/^([A-z\s]){2,}([A-z]){1}$/).test(nom);
 }
+export default class Funcionario
+{
+  constructor(nom, mat, fun) {
+    this.colaborador = nom;
+    this.matricula = mat;
+    this.funcao = fun;
+  }
+  isValidFuncionario() {
+    if(!isValidMatricula(this.matricula)) return false;
+    if(!isValidName(this.colaborador)) return false;
+    return true;
+  }
+}
