4a5c3e10e4bcd02600be89cbf85f0a56b0511019
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-11-24 09:02:58 -0300

    REFACTOR: caminho corrigido do componente

--- src/Composicao/Index.js
+++ src/Composicao/Index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Read from "./Read";
-import Edit from "./Edit";
+import Handle from "./Handle";
 export default function Composicao() {
   return (
     <Routes>
       <Route path="/" element={<Read/>}/>
-      <Route path="/Edit" element={<Edit/>}/>
+      <Route path="/Edit" element={<Handle/>}/>
+      <Route path="/Create" element={<Handle/>}/>
     </Routes>
   );
 }
