792f612bc9797166c6bd71816f8f3650b1e2a3bc
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-01 16:05:33 -0300

    Refactored for inline form design

--- src/_Shared/Filters.js
+++ src/_Shared/Filters.js
@@ -7,22 +7,23 @@ export default function Filters({links, updateVars}) {
   const [reg, setReg] = React.useState(Number.MAX_VALUE);
   return (
     <main className="card p-2 m-2">
-      <div className="d-flex justify-content-around">
-        {links.map((l, i) => ( <>{l}</> ))}
-        <label>Data inicio:</label>
-        <input type="date" value={dataStart} onChange={(a) => { setDataStart(a.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}/>
-        <label>Data final:</label>
-        <input type="date" value={dataStop} onChange={(b) => { setDataStop(b.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}/>
-        <label>Tipo atividade:</label>
-        <select value={ativ} onChange={(c) => { setAtiv(c.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}>
+      <div className="input-group">
+        {links.map((l, i) => ( <span className="input-group-text" key={i}>{l}</span> ))}
+        <span className="input-group-text">Data inicio:</span>
+        <input className="form-control" type="date" value={dataStart} onChange={(a) => { setDataStart(a.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}/>
+        <span className="input-group-text">Data final:</span>
+        <input className="form-control" type="date" value={dataStop} onChange={(b) => { setDataStop(b.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}/>
+        <span className="input-group-text">Tipo atividade:</span>
+        <select className="form-control" value={ativ} onChange={(c) => { setAtiv(c.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}>
           <option value={Number.MAX_VALUE}>TODAS</option>
           {atividade.map((r, i) => ( <option value={i} key={i}>{r}</option> ))}
         </select>
-        <label>Qual Regional:</label>
-        <select value={reg} onChange={(d) => { setReg(d.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}>
+        <span className="input-group-text">Qual Regional:</span>
+        <select className="form-control" value={reg} onChange={(d) => { setReg(d.target.value); updateVars([dataStart, dataStop, ativ, reg]); }}>
           <option value={Number.MAX_VALUE}>TODAS</option>
           {regional.map((r, i) => ( <option value={i} key={i}>{r}</option> ))}
         </select>
+        <button className="btn btn-secondary" type="button" onClick={(e) => {}}>Filtrar</button>
       </div>
     </main>
   );
