15e3e46627bc351274e736b59a2826809a90135b
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-04 14:01:56 -0300

    Generalized retrieval of data through filter

--- src/_Shared/Filters.js
+++ src/_Shared/Filters.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
+import { Requisicao } from "../Requisicao";
 import { regional, atividade } from "../Composicao/Model";
 export default function Filters({links, updateVars}) {
   const [dataStart, setDataStart] = React.useState(Date(Date.now()));
   const [dataStop, setDataStop] = React.useState(Date(Date.now()));
   const [ativ, setAtiv] = React.useState(Number.MAX_VALUE);
   const [reg, setReg] = React.useState(Number.MAX_VALUE);
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    async function getData() {
+      let param = `${dataStart}/${dataStop}/${reg}/${ativ}`;
+      let req = await Requisicao(pathname, "GET", null);
+      let res = req.ok ? await req.json() : [];
+      console.dir(res);
+      updateVars(res);
+    }
+    getData();
+  }, [dataStart, dataStop, ativ, reg]);
   return (
     <main className="card p-2 m-2">
       <div className="input-group">
@@ -23,7 +36,6 @@ export default function Filters({links, updateVars}) {
           <option value={Number.MAX_VALUE}>TODAS</option>
           {regional.map((r, i) => ( <option value={i} key={i}>{r}</option> ))}
         </select>
-        <button className="btn btn-secondary" type="button" onClick={(e) => {}}>Filtrar</button>
       </div>
     </main>
   );
