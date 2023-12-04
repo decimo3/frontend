4d7706187faa8c74a4953bd1939fb20f6163ff22
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-04 14:02:37 -0300

    Added filter component to home page

--- src/Startup.js
+++ src/Startup.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Filters from "./_Shared/Filters";
 export default function Startup()
 {
+  const onUpdateVars = () => {}
   return (
+    <>
+    <Filters links={[]} updateVars={onUpdateVars}/>
     <div className="card p-2 m-2">
       Place for a Dashboard.
     </div>
+    </>
   );
 }
