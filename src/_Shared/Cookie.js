cb502e18958814c3666fcdf034516aad7c15c0a6
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-08 09:57:24 -0300

    Added functions for reading and writing cookies
    ref: https://www.w3schools.com/js/js_cookies.asp

---
+++ src/_Shared/Cookie.js
@@ -0,0 +1,21 @@
+export function setCookie(name, value, exp) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exp*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+export function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
