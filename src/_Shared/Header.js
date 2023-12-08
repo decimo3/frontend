a30bbad3a25d32e92d502bd00e429897b02652cf
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-08 14:07:02 -0300

    FIX path

--- src/_Shared/Header.js
+++ src/_Shared/Header.js
@@ -12,7 +12,7 @@ export default function Header()
           <Link to='/Funcionario'>Funcionários</Link>
           <Link to='/Composicao'>Composições</Link>
           <Link to='/Relatorio'>Relatórios</Link>
-          <Link to='/Autentica'>Usuário</Link>
+          <Link to='/Usuario'>Usuário</Link>
         </nav>
     </header>
   )
