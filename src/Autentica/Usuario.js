8b2a2877a6864d79042c0cf11f27fc4d0e59f805
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-08 09:52:24 -0300

    Added component for user information and changes

---
+++ src/Autentica/Usuario.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Usuario() {
+  // TODO 1: deslogar (sair);
+  // TODO 2: Trocar a senha;
+  // TODO 3: Informações acerca;
+  return (
+    <>
+      <main className="card p-2 m-2">
+
+      </main>
+    </>
+  );
+}
