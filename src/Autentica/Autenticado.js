4fb7c3bbae297f741c3d094ea11d49f3c2489f9a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-11 15:53:32 -0300

    Functional part of the user screen implemented

--- src/Autentica/Autenticado.js
+++ src/Autentica/Autenticado.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import Modal from "../Modal";
 import { getCookie, setCookie } from "../_Shared/Cookie";
 import { Requisicao, errorMsg } from "../Requisicao";
+import { regional, atividade } from "../Composicao/Model";
 export default function Autenticado() {
   // TODO 1: deslogar (sair);
   // TODO 2: Trocar a senha;
   // TODO 3: Informações acerca;
   const History = useNavigate();
   const { state } = useLocation();
-  const [psw1, setPwd1] = React.useState('');
-  const [psw2, setPwd2] = React.useState('');
+  const [usr, setUsr] = React.useState();
   const [showModal, setShowModal] = React.useState(false);
   const [listaAvisos, setListaAviso] = React.useState([]);
   const onCloseModal = () => {
@@ -19,9 +19,15 @@ export default function Autenticado() {
     }
     setShowModal(!showModal);
   }
-  function alterarPalavra() {
-
-  }
+  React.useEffect(() => {
+    async function receberUsuario() {
+      var res = await Requisicao('/Autenticacao');
+      var c = await res.json();
+      console.dir(c);
+      setUsr(c);
+    }
+    receberUsuario();
+  }, [])
   function deslogarUsuario() {
     setCookie("MeuCookie", "", "");
     if (getCookie("MeuCookie")) {
@@ -31,6 +37,13 @@ export default function Autenticado() {
     }
     setShowModal(!showModal);
   }
+  if (!usr) {
+    return (
+      <main className="text-center">
+        Carregando...
+      </main>
+    );
+  }
   return (
     <>
       {showModal && <Modal listaAvisos={listaAvisos} onClose={onCloseModal}/>}
@@ -39,42 +52,37 @@ export default function Autenticado() {
         <hr/>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Matrícula Light:</div>
-          <div className="text-right">2258038</div>
+          <div className="text-right">{usr.matricula}</div>
         </div>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Nome do colaborador:</div>
-          <div className="text-right">Ruan Faustino Camello</div>
+          <div className="text-right">{usr.nome_colaborador}</div>
         </div>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Data de admissão:</div>
-          <div className="text-right">04/09/2023</div>
+          <div className="text-right">{usr.admissao}</div>
         </div>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Situação:</div>
-          <div className="text-right">Ativo</div>
+          <div className="text-right">{usr.situacao}</div>
         </div>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Regional:</div>
-          <div className="text-right">Campo Grande</div>
+          <div className="text-right">{regional[usr.regional]}</div>
         </div>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Função:</div>
-          <div className="text-right">Administrativo</div>
+          <div className="text-right">{usr.funcao}</div>
         </div>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Atividade:</div>
-          <div className="text-right">Programador</div>
+          <div className="text-right">{atividade[usr.atividade]}</div>
         </div>
         <div className="d-flex justify-content-between my-2">
           <div className="text-left">Supervisor:</div>
-          <div className="text-right">Nayara</div>
+          <div className="text-right">{usr.id_supervisor}</div>
         </div>
-        <div className="input-group my-2">
-          <label className="input-group-text">Trocar senha:</label>
-          <input type="password" className="form-control" onChange={(e) => { setPwd1(e.target.value); }} />
-          <input type="password" className="form-control" onChange={(e) => { setPwd2(e.target.value); }} />
-          <input type="button" className="input-group-append btn btn-secondary" value="Trocar" onClick={ ()=>{ alterarPalavra(); }} />
-        </div>
+        <Link to='TrocarSenha' state={usr}>Trocar a senha</Link>
         <input type="button" className="btn btn-warning btn-block my-2" value="Sair" onClick={ ()=>{ deslogarUsuario(); }} />
       </main>
     </>
