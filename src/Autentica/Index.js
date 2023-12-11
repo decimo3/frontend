ea75e21fd072a95c4babd15216db836a400d3303
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-11 15:55:20 -0300

    Added route for user access management

--- src/Autentica/Index.js
+++ src/Autentica/Index.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Autenticar from "./Autenticar";
 import Autenticado from "./Autenticado";
+import TrocarSenha from "./TrocarSenha";
+import Recuperar from "./Recuperar";
 import { getCookie } from "../_Shared/Cookie";
 export default function Usuario()
 {
   return (
       <Routes>
         <Route path="/" element={getCookie("MeuCookie") ? <Autenticado/> : <Autenticar/>} />
+        <Route path="/TrocarSenha" element={<TrocarSenha/>} />
+        <Route path="/Recuperar" element={<Recuperar/>} />
       </Routes>
   );
 }
