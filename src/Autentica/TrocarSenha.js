13664bc43e285cce7b691d25d9f3bd50827300d3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-12 13:11:57 -0300

    1. Validation for changing password; 2. Checking the status of an old or temporary password; 3. Standardization of the filling error notice; 4. Rectified request route; 5. Conditional to display old password field; > if state is valid, the field will not be displayed

--- src/Autentica/TrocarSenha.js
+++ src/Autentica/TrocarSenha.js
@@ -2,18 +2,24 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Modal from "../Modal";
 import { Requisicao, errorMsg } from "../Requisicao";
+import { isValidPalavra } from "../Funcionario/Model";
 class trocarSenha {
   constructor(atual, nova, confirmacao) {
     this.atual = atual;
     this.nova = nova;
     this.confirmacao = confirmacao;
   }
+  isValidTrocarSenha() {
+    if(!isValidPalavra(this.nova)) return false;
+    if(!(this.nova === this.confirmacao)) return false;
+    return true;
+  }
 }
 export default function TrocarSenha() {
   const History = useNavigate();
   const { state } = useLocation();
   const [usr, setUsr] = React.useState();
-  const [psw0, setPwd0] = React.useState('');
+  const [psw0, setPwd0] = React.useState(state ? state.palavra : '');
   const [psw1, setPwd1] = React.useState('');
   const [psw2, setPwd2] = React.useState('');
   const [showModal, setShowModal] = React.useState(false);
@@ -27,14 +33,14 @@ export default function TrocarSenha() {
     let t = new trocarSenha(psw0, psw1, psw2);
     if(!t.isValidTrocarSenha()) {
       setListaAviso([
-        "A senha inserida não é válida!",
-        "Verifique e tente novamente."
+        "Informações inseridas incorretamente!",
+        "Verifique o preenchimento e tente novamente."
       ]);
       setShowModal(!showModal);
       return;
     }
     let data = JSON.stringify(t);
-    let res = await Requisicao("Autenticacao", "PUT", data, null);
+    let res = await Requisicao("/Autenticacao", "PUT", data, null);
     switch(res.status) {
       case 204: setListaAviso(["Senha trocada com sucesso!"]); break;
       case 401: setListaAviso(["A senha anterior não confere!"]); break;
@@ -46,8 +52,10 @@ export default function TrocarSenha() {
     <>
       {showModal && <Modal listaAvisos={listaAvisos} onClose={onCloseModal}/>}
       <main className="card w-50 start-50 translate-middle-x p-4 m-2">
+      {!state && <>
         <label>Senha antiga:</label>
         <input type="password" className="form-control text-right my-2" onChange={(e) => { setPwd0(e.target.value); }} />
+      </>}
         <label>Trocar a senha:</label>
         <input type="password" className="form-control text-right my-2" onChange={(e) => { setPwd1(e.target.value); }} />
         <label>Repita a senha:</label>
