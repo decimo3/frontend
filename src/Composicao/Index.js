43472ce2d9de1025efee409f62d1e15c0adb56e2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-14 10:34:21 -0300

    Display of a lack of authorization message for components that modify the system for unauthorized users

--- src/Composicao/Index.js
+++ src/Composicao/Index.js
@@ -3,14 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import Read from "./Read";
 import Handle from "./Handle";
 import Send from "./Send";
+import { Autorizacao } from "../Requisicao";
+import Proibido from "../_Shared/Proibido";
 export default function Composicao() {
   return (
     <Routes>
       <Route path="/" element={<Read/>}/>
-      <Route path="/Edit" element={<Handle/>}/>
-      <Route path="/Create" element={<Handle/>}/>
-      <Route path="/Send" element={<Send/>}/>
-      <Route path="/Result" element={<Read/>}/>
+      <Route path="/Edit" element={Autorizacao() ? <Handle/> : <Proibido/>} />
+      <Route path="/Create" element={Autorizacao() ? <Handle/> : <Proibido/>} />
+      <Route path="/Send" element={Autorizacao() ? <Send/> : <Proibido/>} />
+      <Route path="/Result" element={Autorizacao() ? <Read/> : <Proibido/>} />
     </Routes>
   );
 }
--- src/Funcionario/Index.js
+++ src/Funcionario/Index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Read from "./Read";
 import Handle from "./Handle";
+import { Autorizacao } from "../Requisicao";
+import Proibido from "../_Shared/Proibido";
 export default function Funcionario()
 {
   return (
       <Routes>
         <Route path="/" element={<Read/>} />
-        <Route path="/Edit" element={<Handle/>} />
-        <Route path="/Create" element={<Handle/>} />
+        <Route path="/Edit" element={Autorizacao() ? <Handle/> : <Proibido/>}  />
+        <Route path="/Create" element={Autorizacao() ? <Handle/> : <Proibido/>} />
       </Routes>
   );
 }
