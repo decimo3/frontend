a02f840c67486047b93124572e19415f3e7d015c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-14 15:03:02 -0300

    Removed cookie if authorization check fails

--- src/Servico.js
+++ src/Servico.js
@@ -1,7 +1,12 @@
 import { Requisicao } from "./Requisicao";
+import { setCookie } from "./_Shared/Cookie";
 export default async function Servico() {
   let req = await Requisicao("/Autenticacao");
-  if(req.status != 200) window.localStorage.removeItem("user");
+  if(req.status != 200) {
+    window.localStorage.removeItem("user");
+    setCookie("MeuCookie", "", "");
+    return;
+  }
   let user = await req.json();
   let u = JSON.stringify(user);
   window.localStorage.setItem("user", u);
