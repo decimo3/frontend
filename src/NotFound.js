ccda9c70741ca8ccd8cc247b9725c32a9698c375
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-14 08:33:24 -0300

    FIX: layout

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,11 +1,12 @@
 import React from "react";
 export default function NotFound() {
   return (
-    <main className="card p-2 m-2">
-      <h1 className="text-danger">Não encontrado!</h1>
+    <main className="card w-50 start-50 translate-middle-x p-2 m-2">
+      <h1 className="text-center text-danger">Não encontrado!</h1>
+      <hr/>
       <p>O link solicitado não foi encontrado!</p>
       <p>Verifique o caminho digitado ou entre em contato com o administrador do sistema.</p>
-      <p><a target="_blank" href='https://api.whatsapp.com/send?phone=5521975429768'>WhatsApp MestreRuan</a></p>
+      <p className="text-center"><a target="_blank" href='https://api.whatsapp.com/send?phone=5521975429768'>WhatsApp MestreRuan</a></p>
     </main>
   );
 }
